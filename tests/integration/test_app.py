9466bccb836d42bfee9da5e22037ade1656f20e5
Nick DeRobertis <whoopnip@gmail.com>
2023-08-21 07:37:46 -0500

    test: Add post test

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -36,6 +36,33 @@ async def test_mirrors_get_request(client: AsyncClient):
     assert response.form is None
 
 
+@pytest.mark.anyio
+async def test_mirrors_post_form_request(client: AsyncClient):
+    params = {
+        "q": "test",
+        "page": 1,
+    }
+    headers = {
+        "x-test-header": "test",
+    }
+    form = {
+        "test": "test",
+    }
+    response = await client.post("/", params=params, headers=headers, data=form)
+    assert response.status_code == 200
+    response = ReflectedResponse(**response.json())
+    assert response.method == "POST"
+    assert _has_headers(headers, response.headers)
+    assert response.cookies is None
+    assert response.json_ is None
+    assert response.body == "test=test"
+    assert urlencode(params) in response.url
+    assert response.form == form
+
+
 def _has_headers(expect_headers: Dict[str, str], headers: Dict[str, str]) -> bool:
     all_headers = {**DEFAULT_HEADERS, **expect_headers}
-    return headers == all_headers
+    for key, value in all_headers.items():
+        if key not in headers or headers[key] != value:
+            return False
+    return True
